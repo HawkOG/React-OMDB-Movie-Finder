9e64d356b3fa229957eb8804d7ead0cad18f9d09
HawkOG <bangerw13@gmail.com>
2019-01-21 08:26:58 -0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+// ENTRY -> OUTPUT
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+          loader: "babel-loader"
+      },{
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname,'public')
+  }
+ }
+
+// JSX Loader
